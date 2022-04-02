7aa1cab67c731a8a581bd0c20e35d9b0142d26f8
naren <naren.tripurana@gmail.com>
2022-04-02 22:31:39 +0530

    Removed depreciated functions

--- base.py
+++ base.py
@@ -5,70 +5,10 @@ import nsetools as nse
 from nsepy import get_history
 from datetime import date, timedelta
 
-def get_data(ticker,start,end):
-    ticker=yf.Ticker(ticker+'.NS')
-    data=ticker.history( start=start, end=end)
-    
-    nifty_data=pd.read_csv('C:/Users/HP/Desktop/Repos/momentum_research/Nifty Historical Data.csv')
-    
-    nifty_2021=pd.read_csv('C:/Users/HP/Desktop/Repos/momentum_research/2021.csv')
-    
-    nifty_historical=nifty_data.append(nifty_2021).reset_index(drop=True)
-    
-    nifty_historical['Date']=pd.to_datetime(nifty_historical['Date'])
-    nifty_historical.index=nifty_historical['Date']
-    
-    shares_traded=nifty_historical['Shares Traded']
-    
-    
-    data=pd.concat([data,shares_traded],axis=1,join='inner')
-    data['Outstanding Shares']=0
-    
-    data.loc[data.index[len(data)-1],'Outstanding Shares']=ticker.get_info()['sharesOutstanding']
-
-    prev_index=data.index[len(data)-1]
-    for index in data[:-1].index[::-1]:
-        if data.loc[prev_index,'Stock Splits']!=0:
-            data.loc[index,'Outstanding Shares']=data.loc[prev_index,'Outstanding Shares']/data.loc[prev_index,'Stock Splits']
-        else:
-            data.loc[index,'Outstanding Shares']=data.loc[prev_index,'Outstanding Shares']
-        prev_index=index
-        
+def get_data(stock):
+    data=pd.read_csv('Historical Price Data/'+stock+'.csv')        
     return data
 
-
-def get_nse_data(ticker,start,end):
-    data=get_history(ticker,start=start,end=end)
-    data.index=pd.to_datetime(data.index)
-    nifty_data=pd.read_csv('C:/Users/HP/Desktop/Repos/momentum_research/Nifty Historical Data.csv')
-    
-    nifty_2021=pd.read_csv('C:/Users/HP/Desktop/Repos/momentum_research/2021.csv')
-    
-    nifty_historical=nifty_data.append(nifty_2021).reset_index(drop=True)
-    
-    nifty_historical['Date']=pd.to_datetime(nifty_historical['Date'])
-    nifty_historical.index=nifty_historical['Date']
-    
-    shares_traded=nifty_historical['Shares Traded']
-    data['Outstanding Shares']=0
-    
-    
-    ticker1=yf.Ticker(ticker+'.NS')
-    
-    stocksplit=ticker1.history(start=start,end=end)['Stock Splits']
-    data=data.join(stocksplit,how='inner')
-    data.loc[data.index[len(data)-1],'Outstanding Shares']=ticker1.get_info()['sharesOutstanding']
-     
-    prev_index=data.index[len(data)-1]
-    for index in data[:-1].index[::-1]:
-        if data.loc[prev_index,'Stock Splits']!=0:
-            data.loc[index,'Outstanding Shares']=data.loc[prev_index,'Outstanding Shares']/data.loc[prev_index,'Stock Splits']
-        else:
-            data.loc[index,'Outstanding Shares']=data.loc[prev_index,'Outstanding Shares']
-        prev_index=index
-        
-    data=pd.concat([data,shares_traded],axis=1,join='inner')        
-    return data
 
 def get_resampled_data(ticker,start,end,period):
     data=get_data(ticker,start=start,end=end)
@@ -91,7 +31,7 @@ def get_resampled_data(ticker,start,end,period):
     if period!='D':
         price_data=data[['Open','Close']].resample(period).last()
         
-        volume_data=data[['Volume','Shares Traded','Outstanding Shares']].resample(period).mean()
+        volume_data=data[['Volume','Outstanding Shares']].resample(period).mean()
         
         data=pd.concat([price_data,volume_data],axis=1,join='inner')
     
@@ -102,11 +42,7 @@ def get_resampled_data(ticker,start,end,period):
     data['Volume Change']=data['Volume'].pct_change()
     data['Log Volume Change']=np.log(data['Volume'].shift(1)/data['Volume']).dropna()
     
-    data['Turnover Rate 1']=data['Volume']/data['Outstanding Shares']
-    data['Inverse Turnover Rate 1']=1/data['Turnover Rate 1']
-    
-    data['Turnover Rate']=data['Volume']/data['Shares Traded']
-    data['Inverse Turnover Rate']=1/data['Turnover Rate']
+    data['Turnover Rate']=data['Volume']/data['Outstanding Shares']
     
     try:
         data['Volatility']=vol
@@ -116,51 +52,6 @@ def get_resampled_data(ticker,start,end,period):
     
     data['Unity']=1
     
-    data.replace([np.inf, -np.inf], np.nan, inplace=True)
-    data.dropna(inplace=True)
-    
-    
-    return data
-
-def get_resampled_nse_data(ticker,start,end,period):
-    data=get_nse_data(ticker,start=start,end=end)
-    
-    data['Returns']= data['Close'].pct_change()
-    data['Log Returns']=np.log(data['Close']/data['Close'].shift(1)).dropna()
-    data.index=pd.to_datetime(data.index)
-    if period=='W':
-        vol=data['Log Returns'].resample('W').std()*np.sqrt(5)
-    
-    if period=='M':
-        vol=data['Log Returns'].resample('M').std()*np.sqrt(20)
-    
-    if period=='Y':
-        vol=data['Log Returns'].resample('Y').std()*np.sqrt(252)
-    
-    price_data=data[['Open','Close']].resample(period).last()
-    
-    volume_data=data[['Volume','Shares Traded','Outstanding Shares']].resample(period).mean()
-    
-    data=pd.concat([price_data,volume_data],axis=1,join='inner')
-    
-    data['Returns']= data['Close'].pct_change()
-    data['Log Returns']=np.log(data['Close']/data['Close'].shift(1)).dropna()
-    
-    
-    data['Volume Change']=data['Volume'].pct_change()
-    data['Log Volume Change']=np.log(data['Volume'].shift(1)/data['Volume']).dropna()
-    
-    
-    data['Turnover Rate']=data['Volume']/data['Shares Traded']
-    data['Inverse Turnover Rate']=1/data['Turnover Rate']
-    
-    data['Turnover Rate 1']=data['Volume']/data['Outstanding Shares']
-    data['Inverse Turnover Rate 1']=data['Volume']/data['Outstanding Shares']
-    
-    data['Volatility']=vol
-    data['Inverse Volatility']=1/vol
-    
-    data['Unity']=1
     data.replace([np.inf, -np.inf], np.nan, inplace=True)
     data.dropna(inplace=True)
     
@@ -468,93 +359,3 @@ def momentum_3(matrix,j,k,equal=True,contrarian=False):
         ins=-ins
         outs=-outs
     return ins,outs
-
-def strategy(investment,ins,outs):
-    historical_returns=pd.DataFrame(columns=outs.columns,index=outs.index)
-    historical_leftover=pd.DataFrame(columns=['BUY','SELL'],index=ins.index)
-    capital_overtime=pd.DataFrame(columns=['Capital'],index=outs.index)
-    
-    for i in range(len(ins)):
-        print(ins.index[i])
-        market_entry=ins.loc[ins.index[i],:]
-        
-        capital_entry=market_entry*investment
-        
-        capital_entry=capital_entry[capital_entry!=0]
-        capital_entry=(-1)*capital_entry
-        stocks_entry=capital_entry.copy()
-        
-        capital_exit=stocks_entry.copy()
-        
-        buy_capital=0
-        sell_capital=0
-        for index in capital_entry.index:
-            capital=(-1)*capital_entry[index]
-            stock_data=get_history(index,start=ins.index[i],end=outs.index[i])
-            if len(stock_data)>0:
-                entry_stock_price=stock_data['Open'][0]
-                
-                no_of_stocks=capital/entry_stock_price
-                no_of_stocks=no_of_stocks
-                
-                stocks_entry[index]=no_of_stocks
-                
-                if capital>0:
-                    buy_capital+=-no_of_stocks*entry_stock_price
-                
-                if capital<0:
-                    sell_capital+=-no_of_stocks*entry_stock_price
-                
-                exit_stock_price=stock_data['Close'][-1]
-                capital_exit[index]=exit_stock_price*no_of_stocks*(-1)
-                
-                capital_entry[index]=no_of_stocks*entry_stock_price
-            else:
-                capital_entry[index]=0
-                capital_exit[index]=0
-                
-        historical_leftover.loc[ins.index[i],'BUY']=buy_capital
-        historical_leftover.loc[ins.index[i],'SELL']=sell_capital
-        returns=capital_exit+capital_entry
-        historical_returns.loc[outs.index[i],returns.index]=returns
-        
-        investment=investment+returns.sum()
-        
-        capital_overtime.loc[outs.index[i],'Capital']=investment
-    return historical_returns,historical_leftover,capital_overtime
-
-def sell_strategy(investment,ins,outs):
-    historical_returns=pd.DataFrame(columns=outs.columns,index=outs.index)
-    
-    for i in range(len(ins)):
-        print(ins.index[i])
-        market_entry=ins.loc[ins.index[i],:]
-        
-        capital_entry=market_entry*investment
-        
-        capital_entry=capital_entry[capital_entry!=0]
-        capital_entry=(-1)*capital_entry
-        
-        capital_returns=capital_entry.copy()
-        
-        sell_capital=0
-        for index in capital_entry.index:
-            capital=capital_entry[index]
-            ticker=yf.Ticker(index+'.NS')
-            stock_data=ticker.history(start=ins.index[i],end=outs.index[i])
-            
-            returns=0
-            for j in stock_data.index:
-                no_of_stocks=capital/stock_data.loc[j,'Open']
-                
-                bought_back=no_of_stocks*stock_data.loc[j,'Close']
-                
-                returns+=capital-bought_back
-                capital=bought_back
-            capital_returns[index]=returns
-            
-        #short_returns=capital_returns.sum()
-        historical_returns.loc[outs.index[i],capital_returns.index]=capital_returns
-        
-        
-    return historical_returns
