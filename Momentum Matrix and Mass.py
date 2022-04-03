6c0182087aab80547e75f96c1084bdf4ad19aa21
naren <naren.tripurana@gmail.com>
2022-04-03 13:38:16 +0530

    Added code and data for Momentum Matrix 3

--- Momentum Matrix and Mass.py
+++ Momentum Matrix and Mass.py
@@ -93,6 +93,43 @@ def momentum_mass(mass,resample,start,end,period):
     return momentum_matrix
 
 
+def momentum_matrix3(velocity,resample,start,end,period):
+    if os.path.exists('Momentum/Momentum Matrix 3/'+period+'/matrix.csv'):
+        for letter in string.ascii_uppercase:
+            if letter>='A':
+                stocks=get_all_stocks()
+                stocks=[a for a in stocks if a.startswith(letter)]
+                momentum_matrix=pd.read_csv('Momentum/Momentum Matrix 3/'+period+'/matrix.csv')
+                
+                momentum_matrix.index=pd.to_datetime(momentum_matrix['Date'])
+                
+                momentum_matrix.drop('Date',axis=1,inplace=True)
+                
+                for stock in stocks:
+                    print(stock)
+                    try:
+                        resampled_data=get_resampled_data(stock,start=start,end=end,period=resample)
+                        momentum=resampled_data[velocity]  
+                        momentum_matrix[stock]=momentum
+                    except:
+                        pass
+                momentum_matrix.to_csv('Momentum/Momentum Matrix 3/'+period+'/matrix.csv')
+    
+    else:
+        stocks=get_all_stocks()
+        stocks=stocks[:1]
+        momentum_matrix=pd.DataFrame()
+        for stock in stocks:
+            print(stock)
+            try:
+                resampled_data=get_resampled_data(stock,start=start,end=end,period=resample)
+                momentum=resampled_data[velocity]  
+                momentum_matrix[stock]=momentum
+            except:
+                pass
+        momentum_matrix.to_csv('Momentum/Momentum Matrix 3/'+period+'/matrix.csv')
+    return momentum_matrix
+
 mass='Turnover Rate'
 velocity='Log Returns'
 start=date(2014,1,1)
@@ -108,3 +145,10 @@ end=date(2022,1,1)
 for period in ['Day','Week','Month','Year']:
     resample=period[0]
     matrix=momentum_mass(mass,resample,start,end,period)
+
+velocity='Log Returns'
+start=date(2014,1,1)
+end=date(2022,1,1)
+for period in ['Day','Week','Month','Year']:
+    resample=period[0]
+    matrix=momentum_matrix3(velocity,resample,start,end,period)
