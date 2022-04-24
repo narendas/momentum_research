7a11fdcb292fb4775687cfdff6378d04a4e19cfe
naren <naren.tripurana@gmail.com>
2022-04-24 13:01:32 +0530

    Bifurcated get_stats to get_stats and get_risks for better risk calculation for true dollar neutral portfolios

--- Daily Portfolios.py
+++ Daily Portfolios.py
@@ -31,12 +31,6 @@ def get_stats(w,l,best_selection,momentum,portfolio,mass):
     d_w={}
     d_l={}
     
-    w_fin=w.loc[w.index[-1],'Capital']/w.loc[w.index[0],'Capital']
-    l_fin=w.loc[l.index[-1],'Capital']/l.loc[l.index[0],'Capital']
-    
-    w_mdd=max_dd(w)
-    l_mdd=max_dd(l)
-    
     w_r=w.resample('M').last()
     l_r=l.resample('M').last()
     w_r=100*np.log(w_r/w_r.shift(1)).dropna()
@@ -48,12 +42,6 @@ def get_stats(w,l,best_selection,momentum,portfolio,mass):
     w_std=w_r.std()[0]
     l_std=l_r.std()[0]
     
-    w_sp=w_mean/w_std
-    l_sp=l_mean/l_std
-    
-    w_var=var_historic(w_r)[0]
-    l_var=var_historic(l_r)[0]
-    
     d_w['Momentum']=momentum
     d_w['Mass']=mass
     d_w['Strategy']=best_selection[0]
@@ -62,10 +50,6 @@ def get_stats(w,l,best_selection,momentum,portfolio,mass):
     
     d_w['Mean']=w_mean
     d_w['Std']=w_std
-    d_w['Fin Wealth']=w_fin
-    d_w['Sharpe Ratio']=np.abs(w_sp)
-    d_w['Var 95%']=w_var
-    d_w['Max DD']=w_mdd
     
     d_l['Momentum']=momentum
     d_l['Mass']=mass
@@ -75,27 +59,15 @@ def get_stats(w,l,best_selection,momentum,portfolio,mass):
     
     d_l['Mean']=l_mean
     d_l['Std']=l_std
-    d_l['Fin Wealth']=l_fin
-    d_l['Sharpe Ratio']=np.abs(l_sp)
-    d_l['Var 95%']=l_var
-    d_l['Max DD']=l_mdd
     
     if best_selection[0]=='Strategy':
         df=w-l
-        mdd=max_dd(df)
-        
-        fin=w_fin-l_fin
-        
         df=df.resample('M').last()
         df=100*np.log(df/df.shift(1)).dropna()
+        
         mean=w_mean-l_mean
         std=df.std()[0]
         
-        sp=mean/std
-        var=var_historic(df)[0]
-    
-        d={}
-        
         d['Momentum']=momentum
         d['Mass']=mass
         d['Strategy']=best_selection[0]
@@ -104,26 +76,15 @@ def get_stats(w,l,best_selection,momentum,portfolio,mass):
         
         d['Mean']=mean
         d['Std']=std
-        d['Fin Wealth']=fin
-        d['Sharpe Ratio']=np.abs(sp)
-        d['Var 95%']=var
-        d['Max DD']=mdd
     
     elif best_selection[0]=='Contrarian Strategy':
-        df=w-l
-        mdd=max_dd(df)
-        
-        fin=l_fin-w_fin
-        
+        df=l-w
         df=df.resample('M').last()
         df=100*np.log(df/df.shift(1)).dropna()
+        
         mean=l_mean-w_mean
         std=df.std()[0]
-        
-        sp=mean/std
-        var=var_historic(df)[0]
     
-        d={}
         
         d['Momentum']=momentum
         d['Mass']=mass
@@ -133,16 +94,40 @@ def get_stats(w,l,best_selection,momentum,portfolio,mass):
         
         d['Mean']=mean
         d['Std']=std
-    
-        d['Fin Wealth']=fin
-        d['Sharpe Ratio']=np.abs(sp)
-        d['Var 95%']=var
-        d['Max DD']=mdd
     return d_w,d_l,d
 
-final_df=pd.DataFrame(columns=['Momentum','Mass','Strategy',
-                               'Criterion','Portfolio','Mean',
-                               'Std','Fin Wealth','Sharpe Ratio',
+def get_risks(df,best_selection,momentum,portfolio,mass):
+    mdd=max_dd(df)
+    
+    fin=df.loc[df.index[-1],'Capital']/df.loc[df.index[0],'Capital']
+    
+    df=df.resample('M').last()
+    df=100*np.log(df/df.shift(1)).dropna()
+    mean=df.mean()[0]
+    std=df.std()[0]
+    
+    sp=mean/std
+    var=var_historic(df)[0]
+
+    d={}
+    
+    d['Momentum']=momentum
+    d['Mass']=mass
+    d['Strategy']=best_selection[0]
+    d['Criterion']=best_selection[2]
+    d['Portfolio']=portfolio
+    
+    d['Fin Wealth']=fin
+    d['Sharpe Ratio']=np.abs(sp)
+    d['Var 95%']=var
+    d['Max DD']=mdd
+    return d
+    
+stats_df=pd.DataFrame(columns=['Momentum','Mass','Strategy','Criterion',
+                               'Portfolio','Mean','Std'])
+
+risks_df=pd.DataFrame(columns=['Momentum','Mass','Strategy',
+                               'Criterion','Portfolio','Fin Wealth','Sharpe Ratio',
                                'Var 95%','Max DD'])
 # =============================================================================
 # MOMENTUM 1
@@ -162,22 +147,25 @@ if m1_tr_best[1]=='W-L':
     m1_tr_r=m1_tr.resample('M').last()
     m1_tr_r=100*np.log(m1_tr_r/m1_tr_r.shift(1)).dropna()
     d['Std']=m1_tr_r.std()[0]
-    d['Sharpe Ratio']=d['Mean']/d['Std']
-    final_df=pd.concat([final_df,pd.DataFrame(d_w.values(),index=d_w.keys()).T],ignore_index=True)
-    final_df=pd.concat([final_df,pd.DataFrame(d_l.values(),index=d_l.keys()).T],ignore_index=True)
-    final_df=pd.concat([final_df,pd.DataFrame(d.values(),index=d.keys()).T],ignore_index=True)
 
+    stats_df=pd.concat([stats_df,pd.DataFrame(d_w.values(),index=d_w.keys()).T],ignore_index=True)
+    stats_df=pd.concat([stats_df,pd.DataFrame(d_l.values(),index=d_l.keys()).T],ignore_index=True)
+    stats_df=pd.concat([stats_df,pd.DataFrame(d.values(),index=d.keys()).T],ignore_index=True)
+    
+    d=get_risks(m1_tr,m1_tr_best,'Momentum 1','W-L','Turnover Rate')
+    risks_df=pd.concat([risks_df,pd.DataFrame(d.values(),index=d.keys()).T],ignore_index=True)
 else:
     d_w,d_l,d=get_stats(m1_tr_w, m1_tr_l,m1_tr_best,'Momentum 1','L-W','Turnover Rate')
     m1_tr_r=m1_tr.resample('M').last()
     m1_tr_r=100*np.log(m1_tr_r/m1_tr_r.shift(1)).dropna()
     d['Std']=m1_tr_r.std()[0]
-    d['Sharpe Ratio']=d['Mean']/d['Std']
     
-    final_df=pd.concat([final_df,pd.DataFrame(d_w.values(),index=d_w.keys()).T],ignore_index=True)
-    final_df=pd.concat([final_df,pd.DataFrame(d_l.values(),index=d_l.keys()).T],ignore_index=True)
-    final_df=pd.concat([final_df,pd.DataFrame(d.values(),index=d.keys()).T],ignore_index=True)
-
+    stats_df=pd.concat([stats_df,pd.DataFrame(d_w.values(),index=d_w.keys()).T],ignore_index=True)
+    stats_df=pd.concat([stats_df,pd.DataFrame(d_l.values(),index=d_l.keys()).T],ignore_index=True)
+    stats_df=pd.concat([stats_df,pd.DataFrame(d.values(),index=d.keys()).T],ignore_index=True)
+    
+    d=get_risks(m1_tr,m1_tr_best,'Momentum 1','L-W','Turnover Rate')
+    risks_df=pd.concat([risks_df,pd.DataFrame(d.values(),index=d.keys()).T],ignore_index=True)
 
 '''INVERSE TURNOVER RATE'''
 m1_itr=pd.read_csv(os.path.join(m1_itr_best[0],'Momentum 1','Inverse Turnover Rate',m1_itr_best[1],period,m1_itr_best[2]+'.csv'),index_col='Date')
@@ -194,23 +182,25 @@ if m1_itr_best[1]=='W-L':
     m1_itr_r=m1_itr.resample('M').last()
     m1_itr_r=100*np.log(m1_itr_r/m1_itr_r.shift(1)).dropna()
     d['Std']=m1_itr_r.std()[0]
-    d['Sharpe Ratio']=d['Mean']/d['Std']
     
-    final_df=pd.concat([final_df,pd.DataFrame(d_w.values(),index=d_w.keys()).T],ignore_index=True)
-    final_df=pd.concat([final_df,pd.DataFrame(d_l.values(),index=d_l.keys()).T],ignore_index=True)
-    final_df=pd.concat([final_df,pd.DataFrame(d.values(),index=d.keys()).T],ignore_index=True)
-
+    stats_df=pd.concat([stats_df,pd.DataFrame(d_w.values(),index=d_w.keys()).T],ignore_index=True)
+    stats_df=pd.concat([stats_df,pd.DataFrame(d_l.values(),index=d_l.keys()).T],ignore_index=True)
+    stats_df=pd.concat([stats_df,pd.DataFrame(d.values(),index=d.keys()).T],ignore_index=True)
+    
+    d=get_risks(m1_itr,m1_itr_best,'Momentum 1','W-L','Inverse Turnover Rate')
+    risks_df=pd.concat([risks_df,pd.DataFrame(d.values(),index=d.keys()).T],ignore_index=True)
 else:
     d_w,d_l,d=get_stats(m1_itr_w, m1_itr_l,m1_itr_best,'Momentum 1','L-W','Inverse Turnover Rate')
     m1_itr_r=m1_itr.resample('M').last()
     m1_itr_r=100*np.log(m1_itr_r/m1_itr_r.shift(1)).dropna()
     d['Std']=m1_itr_r.std()[0]
-    d['Sharpe Ratio']=d['Mean']/d['Std']
     
-    final_df=pd.concat([final_df,pd.DataFrame(d_w.values(),index=d_w.keys()).T],ignore_index=True)
-    final_df=pd.concat([final_df,pd.DataFrame(d_l.values(),index=d_l.keys()).T],ignore_index=True)
-    final_df=pd.concat([final_df,pd.DataFrame(d.values(),index=d.keys()).T],ignore_index=True)
-
+    stats_df=pd.concat([stats_df,pd.DataFrame(d_w.values(),index=d_w.keys()).T],ignore_index=True)
+    stats_df=pd.concat([stats_df,pd.DataFrame(d_l.values(),index=d_l.keys()).T],ignore_index=True)
+    stats_df=pd.concat([stats_df,pd.DataFrame(d.values(),index=d.keys()).T],ignore_index=True)
+    
+    d=get_risks(m1_itr,m1_itr_best,'Momentum 1','L-W','Inverse Turnover Rate')
+    risks_df=pd.concat([risks_df,pd.DataFrame(d.values(),index=d.keys()).T],ignore_index=True)
 
 # =============================================================================
 # MOMENTUM 2
@@ -230,23 +220,25 @@ if m2_tr_best[1]=='W-L':
     m2_tr_r=m2_tr.resample('M').last()
     m2_tr_r=100*np.log(m2_tr_r/m2_tr_r.shift(1)).dropna()
     d['Std']=m2_tr_r.std()[0]
-    d['Sharpe Ratio']=d['Mean']/d['Std']
 
-    final_df=pd.concat([final_df,pd.DataFrame(d_w.values(),index=d_w.keys()).T],ignore_index=True)
-    final_df=pd.concat([final_df,pd.DataFrame(d_l.values(),index=d_l.keys()).T],ignore_index=True)
-    final_df=pd.concat([final_df,pd.DataFrame(d.values(),index=d.keys()).T],ignore_index=True)
-
+    stats_df=pd.concat([stats_df,pd.DataFrame(d_w.values(),index=d_w.keys()).T],ignore_index=True)
+    stats_df=pd.concat([stats_df,pd.DataFrame(d_l.values(),index=d_l.keys()).T],ignore_index=True)
+    stats_df=pd.concat([stats_df,pd.DataFrame(d.values(),index=d.keys()).T],ignore_index=True)
+    
+    d=get_risks(m2_tr,m2_tr_best,'Momentum 2','W-L','Turnover Rate')
+    risks_df=pd.concat([risks_df,pd.DataFrame(d.values(),index=d.keys()).T],ignore_index=True)
 else:
     d_w,d_l,d=get_stats(m2_tr_w, m2_tr_l,m2_tr_best,'Momentum 2','L-W','Turnover Rate')
     m2_tr_r=m2_tr.resample('M').last()
     m2_tr_r=100*np.log(m2_tr_r/m2_tr_r.shift(1)).dropna()
     d['Std']=m2_tr_r.std()[0]
-    d['Sharpe Ratio']=d['Mean']/d['Std']
     
-    final_df=pd.concat([final_df,pd.DataFrame(d_w.values(),index=d_w.keys()).T],ignore_index=True)
-    final_df=pd.concat([final_df,pd.DataFrame(d_l.values(),index=d_l.keys()).T],ignore_index=True)
-    final_df=pd.concat([final_df,pd.DataFrame(d.values(),index=d.keys()).T],ignore_index=True)
-
+    stats_df=pd.concat([stats_df,pd.DataFrame(d_w.values(),index=d_w.keys()).T],ignore_index=True)
+    stats_df=pd.concat([stats_df,pd.DataFrame(d_l.values(),index=d_l.keys()).T],ignore_index=True)
+    stats_df=pd.concat([stats_df,pd.DataFrame(d.values(),index=d.keys()).T],ignore_index=True)
+    
+    d=get_risks(m2_tr,m2_tr_best,'Momentum 2','L-W','Turnover Rate')
+    risks_df=pd.concat([risks_df,pd.DataFrame(d.values(),index=d.keys()).T],ignore_index=True)
 
 '''INVERSE TURNOVER RATE'''
 m2_itr=pd.read_csv(os.path.join(m2_itr_best[0],'Momentum 2','Inverse Turnover Rate',m2_itr_best[1],period,m2_itr_best[2]+'.csv'),index_col='Date')
@@ -263,23 +255,25 @@ if m2_itr_best[1]=='W-L':
     m2_itr_r=m2_itr.resample('M').last()
     m2_itr_r=100*np.log(m2_itr_r/m2_itr_r.shift(1)).dropna()
     d['Std']=m2_itr_r.std()[0]
-    d['Sharpe Ratio']=d['Mean']/d['Std']
     
-    final_df=pd.concat([final_df,pd.DataFrame(d_w.values(),index=d_w.keys()).T],ignore_index=True)
-    final_df=pd.concat([final_df,pd.DataFrame(d_l.values(),index=d_l.keys()).T],ignore_index=True)
-    final_df=pd.concat([final_df,pd.DataFrame(d.values(),index=d.keys()).T],ignore_index=True)
-
+    stats_df=pd.concat([stats_df,pd.DataFrame(d_w.values(),index=d_w.keys()).T],ignore_index=True)
+    stats_df=pd.concat([stats_df,pd.DataFrame(d_l.values(),index=d_l.keys()).T],ignore_index=True)
+    stats_df=pd.concat([stats_df,pd.DataFrame(d.values(),index=d.keys()).T],ignore_index=True)
+    
+    d=get_risks(m2_itr,m2_itr_best,'Momentum 2','W-L','Inverse Turnover Rate')
+    risks_df=pd.concat([risks_df,pd.DataFrame(d.values(),index=d.keys()).T],ignore_index=True)
 else:
     d_w,d_l,d=get_stats(m2_itr_w, m2_itr_l,m2_itr_best,'Momentum 2','L-W','Inverse Turnover Rate')
     m2_itr_r=m2_itr.resample('M').last()
     m2_itr_r=100*np.log(m2_itr_r/m2_itr_r.shift(1)).dropna()
     d['Std']=m2_itr_r.std()[0]
-    d['Sharpe Ratio']=d['Mean']/d['Std']
     
-    final_df=pd.concat([final_df,pd.DataFrame(d_w.values(),index=d_w.keys()).T],ignore_index=True)
-    final_df=pd.concat([final_df,pd.DataFrame(d_l.values(),index=d_l.keys()).T],ignore_index=True)
-    final_df=pd.concat([final_df,pd.DataFrame(d.values(),index=d.keys()).T],ignore_index=True)
-
+    stats_df=pd.concat([stats_df,pd.DataFrame(d_w.values(),index=d_w.keys()).T],ignore_index=True)
+    stats_df=pd.concat([stats_df,pd.DataFrame(d_l.values(),index=d_l.keys()).T],ignore_index=True)
+    stats_df=pd.concat([stats_df,pd.DataFrame(d.values(),index=d.keys()).T],ignore_index=True)
+    
+    d=get_risks(m2_itr,m2_itr_best,'Momentum 2','L-W','Inverse Turnover Rate')
+    risks_df=pd.concat([risks_df,pd.DataFrame(d.values(),index=d.keys()).T],ignore_index=True)
 # =============================================================================
 # MOMENTUM 3
 # =============================================================================
@@ -293,27 +287,29 @@ m3_l=pd.read_csv(os.path.join(m3_best[0],'Momentum 3','L',period,m3_best[2]+'.cs
 m3_l.index=pd.to_datetime(m3_l.index)
 
 if m3_best[1]=='W-L':
-    d_w,d_l,d=get_stats(m3_w, m3_l,m3_best,'Momentum 3','W-L','Turnover Rate')
+    d_w,d_l,d=get_stats(m3_w, m3_l,m3_best,'Momentum 3','W-L','Inverse Volatility')
     m3_r=m3.resample('M').last()
     m3_r=100*np.log(m3_r/m3_r.shift(1)).dropna()
     d['Std']=m3_r.std()[0]
-    d['Sharpe Ratio']=d['Mean']/d['Std']
     
-    final_df=pd.concat([final_df,pd.DataFrame(d_w.values(),index=d_w.keys()).T],ignore_index=True)
-    final_df=pd.concat([final_df,pd.DataFrame(d_l.values(),index=d_l.keys()).T],ignore_index=True)
-    final_df=pd.concat([final_df,pd.DataFrame(d.values(),index=d.keys()).T],ignore_index=True)
-
+    stats_df=pd.concat([stats_df,pd.DataFrame(d_w.values(),index=d_w.keys()).T],ignore_index=True)
+    stats_df=pd.concat([stats_df,pd.DataFrame(d_l.values(),index=d_l.keys()).T],ignore_index=True)
+    stats_df=pd.concat([stats_df,pd.DataFrame(d.values(),index=d.keys()).T],ignore_index=True)
+    
+    d=get_risks(m3,m3_best,'Momentum 3','W-L','Inverse Volatility')
+    risks_df=pd.concat([risks_df,pd.DataFrame(d.values(),index=d.keys()).T],ignore_index=True)
 else:
-    d_w,d_l,d=get_stats(m3_w, m3_l,m3_best,'Momentum 3','L-W','Turnover Rate')
+    d_w,d_l,d=get_stats(m3_w, m3_l,m3_best,'Momentum 3','L-W','Inverse Volatility')
     m3_r=m3.resample('M').last()
     m3_r=100*np.log(m3_r/m3_r.shift(1)).dropna()
     d['Std']=m3_r.std()[0]
-    d['Sharpe Ratio']=d['Mean']/d['Std']
     
-    final_df=pd.concat([final_df,pd.DataFrame(d_w.values(),index=d_w.keys()).T],ignore_index=True)
-    final_df=pd.concat([final_df,pd.DataFrame(d_l.values(),index=d_l.keys()).T],ignore_index=True)
-    final_df=pd.concat([final_df,pd.DataFrame(d.values(),index=d.keys()).T],ignore_index=True)
-
+    stats_df=pd.concat([stats_df,pd.DataFrame(d_w.values(),index=d_w.keys()).T],ignore_index=True)
+    stats_df=pd.concat([stats_df,pd.DataFrame(d_l.values(),index=d_l.keys()).T],ignore_index=True)
+    stats_df=pd.concat([stats_df,pd.DataFrame(d.values(),index=d.keys()).T],ignore_index=True)
+    
+    d=get_risks(m3,m3_best,'Momentum 3','L-W','Inverse Volatility')
+    risks_df=pd.concat([risks_df,pd.DataFrame(d.values(),index=d.keys()).T],ignore_index=True)
 ticker='^NSEI'
 ticker=yf.Ticker(ticker)
 nifty=ticker.history(start=date(2014,1,1),end=date(2022,1,1,))
